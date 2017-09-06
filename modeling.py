2be5cf8fc5335c87d8ff946784eb6894012fc5dc
Junhee <whwnsgml48@gmail.com>
2017-09-06 15:54:28 +0900

    ver 4

--- modeling.py
+++ modeling.py
@@ -1,44 +1,54 @@
 import tensorflow as tf
-
 import numpy as np
-from formula import X_train, X_test, Y_train, Y_test
-
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
+train = pd.read_csv('C:/Users/JunHee/Desktop/dd/test/Train.csv', encoding = 'cp949')
+test = pd.read_csv('C:/Users/JunHee/Desktop/dd/test/Validation.csv', encoding = 'utf8')
+#모델 파라미터 튜닝을 위해 validation dataset을 test로 사용한다. 
 
-def plot_delta(Y_test, Y_pred):
-    x = np.linspace(0, 1, 200)
-    fig, ax = plt.subplots()
-    y = Y_test - Y_pred
-    ax.fill(x, y, zorder=10)
-    ax.grid(True, zorder=5)
-    plt.show()
+col_list = ['Play_month', 'Critic_star', 'Netizen_star_before_play', 'Num_of_participants_before_play','Expect_index_wannasee', 'Expect_index_notreally',
+           'Week1_isplay','Week1_playdays','Week1_holiday','Week1_audience']
+#사용할 column은 다음과 같다. 
+
+#변수 할당
+train = train[col_list]
+test = test[col_list]
+
+X_train = train.iloc[:,:-1]
+Y_train = train.iloc[:,-1]
+Y_train = pd.DataFrame(Y_train)
+
+X_test = test.iloc[:,:-1]
+Y_test = test.iloc[:,-1]
+Y_test = pd.DataFrame(Y_test)
 
 total_len = X_train.shape[0]
 
-# Parameters
-learning_rate = 0.001
-training_epochs = 500
+# 모델 Parameters 세팅
+learning_rate = 0.01
+training_epochs = 50
 batch_size = 10
 display_step = 1
 dropout_rate = 0.9
 
 # Network Parameters
+total_len = X_train.shape[0]
 n_hidden_1 = 32 # 1st layer number of features
 n_hidden_2 = 200 # 2nd layer number of features
 n_hidden_3 = 200
 n_hidden_4 = 256
-n_input = X_train.shape[1]
+n_input = X_train.shape[1]#input의 개수를 X_train 데이터에 맞춰준다.
 n_classes = 1
 
 # tf Graph input
 
-x = tf.placeholder("float", [None, 2])
+x = tf.placeholder("float", [None, n_input])
 y = tf.placeholder("float", [None, 1])
 
 
-# Create model
+# Create model : 여기서 모델은 hidden layer가 4개로 설정됨. 
 def model(x, weights, biases):
     with tf.name_scope("dnn"):
         layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
@@ -53,6 +63,7 @@ def model(x, weights, biases):
         return out_layer
 
 # Store layers weight & bias
+# layers의 parameter는 초기에 난수로 할당한다.(mean : 0, sd = 0.1)
 with tf.name_scope("weights-biases"):
     weights = {
         'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1], 0, 0.1)),
@@ -69,17 +80,19 @@ with tf.name_scope("weights-biases"):
         'out': tf.Variable(tf.random_normal([n_classes], 0, 0.1))
     }
 
-# Construct model
+# 변수설정 및 모델 파라미터 설정 완료 
+
+# 모델 생성
 with tf.name_scope("train"):
     y_pred = model(x, weights, biases)
-    cost = tf.reduce_mean(tf.square(y_pred-y))
+    cost = tf.reduce_mean(tf.square(y_pred-y))#rmse를 cost로 설정
     optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
-# Launch the graph
+# 모델 실행
 with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
+    sess.run(tf.global_variables_initializer())#변수 초기화 실시
 
-    # Training cycle
+    # 모델 훈련 과정
     for epoch in range(training_epochs):
         avg_cost = 0.
         total_batch = int(total_len/batch_size)
@@ -88,13 +101,16 @@ with tf.Session() as sess:
             batch_x = X_train[i*batch_size:(i+1)*batch_size]
             batch_y = Y_train[i*batch_size:(i+1)*batch_size]
             # Run optimization op (backprop) and cost op (to get loss value)
-            _, c, p = sess.run([optimizer, cost, y_pred], feed_dict={x: batch_x,
-                                                          y: batch_y})
+            print(batch_x)
+            print(batch_y)
+            _, c, p = sess.run([optimizer, cost, y_pred], feed_dict={x: batch_x, y: batch_y})
+            print('dd')
             # Compute average loss
             avg_cost += c / total_batch
 
         # sample prediction
-        label_value = batch_y
+
+        label_value = batch_y['Week1_audience'].tolist()
         estimate = p
         err = label_value-estimate
         print ("num batch:", total_batch)
@@ -111,4 +127,3 @@ with tf.Session() as sess:
     accuracy = sess.run(cost, feed_dict={x: X_test, y: Y_test})
     predicted_vals = sess.run(y_pred, feed_dict={x: X_test})
     print ("Accuracy:", accuracy)
-    plot_delta(Y_test=Y_test, Y_pred=predicted_vals)
--- neural_network.py
+++ neural_network.py
@@ -1,74 +1,124 @@
-import pandas as pd 
-import numpy
-import tensorflow as tf 
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.wrappers.scikit_learn import KerasRegressor
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import KFold
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import Pipeline
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+
 
 def data_load(file_name, col_list):
-	'''
+    '''
 	데이터를 로드하는 함수 
 	y에 해당하는 데이터는 데이터프레임 마지막에 위치해야함. 
 	x, y를 반환함. 
 	'''
-	data = pd.read_csv(file_name, encoding = 'cp949')
-	data = data[col_list]
-	data=data.fillna(0)
-	data_x = data.iloc[:,:-1]
-	data_y = data.iloc[:,-1]
-	data_y = pd.DataFrame(data_y)
+    data = pd.read_csv(file_name, encoding='cp949')
+    data = data[col_list]
+    data = data.fillna(0)
+    data_x = data.iloc[:, :-1]
+    data_y = data.iloc[:, -1]
+    data_y = pd.DataFrame(data_y)
 
-	return data_x, data_y
-'''
-def DNN_regression(weights, biases):
-	#using tensorflow 
-	#depth : 4
-	with tf.name_scope("dnn"):
+    return data_x, data_y
+
+
+def DNN_regression(x, weights, biases):
+    # using tensorflow
+    # depth : 4
+    with tf.name_scope("dnn"):
         layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
-        layer_1 = tf.nn.relu(layer_1)
-        layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
-        layer_2 = tf.nn.relu(layer_2)
-        layer_3 = tf.add(tf.matmul(layer_2, weights['h3']), biases['b3'])
-        layer_3 = tf.nn.relu(layer_3)
-        layer_4 = tf.add(tf.matmul(layer_3, weights['h4']), biases['b4'])
-        layer_4 = tf.nn.relu(layer_4)
-        out_layer = tf.matmul(layer_4, weights['out']) + biases['out']
-        return out_layer
+    layer_1 = tf.nn.relu(layer_1)
+    layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
+    layer_2 = tf.nn.relu(layer_2)
+    layer_3 = tf.add(tf.matmul(layer_2, weights['h3']), biases['b3'])
+    layer_3 = tf.nn.relu(layer_3)
+    layer_4 = tf.add(tf.matmul(layer_3, weights['h4']), biases['b4'])
+    layer_4 = tf.nn.relu(layer_4)
+    out_layer = tf.matmul(layer_4, weights['out']) + biases['out']
+    return out_layer
 
-'''
-
-
-def DNN_regression_(depth, input_size):
-	'''
-	using keras 
-	depth는 자유조정 
-	'''
-	model = Sequential()
-	model.add(Dense(depth, input_dim = input_size, kernel_initializer = 'normal',
-		activation = 'relu'))
-	model.add(Dense(1, kernel_initializer = 'normal'))
-	model.compile(loss = 'mean_squared_error', optimizer = 'adam')
-
-	return model
 
 def main():
-	col_list = ['Play_month', 'Critic_star', 'Netizen_star_before_play', 'Num_of_participants_before_play','Expect_index_wannasee', 'Expect_index_notreally',
-           'Week1_isplay','Week1_playdays','Week1_holiday','Week1_audience']
-	train_x, train_y = data_load('C:/Users/JunHee/Desktop/dd/test/Train.csv', col_list)
-	estimators = []
-	estimators.append(('Standardize', StandardScaler()))
-	estimators.append(('mip', KerasRegressor(build_fn = DNN_regression_(20,9), epochs = 100, batch_size = 5,
-		verbose = 0)))
-	pipeline = Pipeline(estimators) 
-	seed = 7
-	numpy.random.seed(seed)
-	kfold = KFold(n_splits = 10, random_state = seed)
-	results = cross_val_score(pipeline, train_x, train_y, cv=kfold)
-	print("Wider: %.2f (%.2f) MSE" % (results.mean(), results.std()))
+    col_list = ['Play_month', 'Critic_star', 'Netizen_star_before_play', 'Num_of_participants_before_play',
+                'Expect_index_wannasee', 'Expect_index_notreally',
+                'Week1_isplay', 'Week1_playdays', 'Week1_holiday', 'Week1_audience']
+    X_train, Y_train = data_load('C:/Users/JunHee/Desktop/dd/test/Train.csv', col_list)
+    X_test, Y_test = data_load('C:/Users/JunHee/Desktop/dd/test/Train.csv', col_list)
+
+    # 모델 Parameters 세팅
+    learning_rate = 0.01
+    training_epochs = 50
+    batch_size = 10
+    display_step = 1
+    dropout_rate = 0.9
+
+    # Network Parameters
+    total_len = X_train.shape[0]
+    n_hidden_1 = 32  # 1st layer number of features
+    n_hidden_2 = 200  # 2nd layer number of features
+    n_hidden_3 = 200
+    n_hidden_4 = 256
+    n_input = X_train.shape[1]  # input의 개수를 X_train 데이터에 맞춰준다.
+    n_classes = 1
+
+    x = tf.placeholder("float", [None, n_input])
+    y = tf.placeholder("float", [None, 1])
+
+    with tf.name_scope('weight-bias'):
+        weights = {
+            'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1], 0, 0.1)),
+            'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2], 0, 0.1)),
+            'h3': tf.Variable(tf.random_normal([n_hidden_2, n_hidden_3], 0, 0.1)),
+            'h4': tf.Variable(tf.random_normal([n_hidden_3, n_hidden_4], 0, 0.1)),
+            'out': tf.Variable(tf.random_normal([n_hidden_4, n_classes], 0, 0.1))
+        }
+        biases = {
+            'b1': tf.Variable(tf.random_normal([n_hidden_1], 0, 0.1)),
+            'b2': tf.Variable(tf.random_normal([n_hidden_2], 0, 0.1)),
+            'b3': tf.Variable(tf.random_normal([n_hidden_3], 0, 0.1)),
+            'b4': tf.Variable(tf.random_normal([n_hidden_4], 0, 0.1)),
+            'out': tf.Variable(tf.random_normal([n_classes], 0, 0.1))
+        }
+
+    # 모델 생성
+    with tf.name_scope("train"):
+        y_pred = DNN_regression(x, weights, biases)
+        cost = tf.reduce_mean(tf.square(y_pred - y))  # rmse를 cost로 설정
+        optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+    # 모델 실행
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())  # 변수 초기화 실시
+
+        # 모델 훈련 과정
+        for epoch in range(training_epochs):
+            avg_cost = 0.
+            total_batch = int(total_len / batch_size)
+            # Loop over all batches
+            for i in range(total_batch - 1):
+                batch_x = X_train[i * batch_size : (i + 1) * batch_size]
+                batch_y = Y_train[i * batch_size : (i + 1) * batch_size]
+                # Run optimization op (backprop) and cost op (to get loss value)
+                _, c, p = sess.run([optimizer, cost, y_pred], feed_dict={x: batch_x, y: batch_y})
+                # Compute average loss
+                avg_cost += c / total_batch
+            # sample prediction
+
+            label_value = batch_y['Week1_audience'].tolist()
+            estimate = p
+            err = label_value - estimate
+            print("num batch:", total_batch)
+
+            # Display logs per epoch step
+            if epoch % display_step == 0:
+                print("Epoch:", '%04d' % (epoch + 1), "cost=", "{:.9f}".format(avg_cost))
+                print("[*]----------------------------")
+                for i in range(10):
+                    print("label value:", label_value[i], "estimated value:", estimate[i])
+                print("[*]============================")
+
+        # Test model
+        accuracy = sess.run(cost, feed_dict={x: X_test, y: Y_test})
+        predicted_vals = sess.run(y_pred, feed_dict={x: X_test})
+        print("Accuracy:", accuracy)
+
 
 if __name__ == '__main__':
-	main()
+    main()
